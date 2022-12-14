2a78ce0d6effe2cf6be2a53ff89393143f3d1503
Tubboge_ (Pepe Wightmore) <pepe.wightmore@protonmail.com>
2022-12-14 15:56:56 +0000

    more cmds

--- commands/img.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require ('discord.js');
-module.exports = {
-    data:new SlashCommandBuilder()
-        .setName('whoasked')
-        .setDescription('Tell someone that nobody asked'),
-    async execute(interaction) {
-        await interaction.reply('Nobody asked.')
-    }
-}
